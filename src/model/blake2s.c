65fe0d6c5b4157e264723de2d03a6121054e142f
Joachim Strömbergson <joachim@assured.se>
2022-02-10 11:03:37 +0100

    Change macro into function to allow debug outputs

--- src/model/blake2s.c
+++ src/model/blake2s.c
@@ -1,93 +1,119 @@
+//======================================================================
+//
 // blake2s.c
+// ---------
+//
 // A simple blake2s Reference Implementation.
+//======================================================================
 
 #include "blake2s.h"
 
-// Cyclic right rotation.
 
+// Cyclic right rotation.
 #ifndef ROTR32
 #define ROTR32(x, y)  (((x) >> (y)) ^ ((x) << (32 - (y))))
 #endif
 
-// Little-endian byte access.
 
+// Little-endian byte access.
 #define B2S_GET32(p)                            \
     (((uint32_t) ((uint8_t *) (p))[0]) ^        \
     (((uint32_t) ((uint8_t *) (p))[1]) << 8) ^  \
     (((uint32_t) ((uint8_t *) (p))[2]) << 16) ^ \
     (((uint32_t) ((uint8_t *) (p))[3]) << 24))
 
-// Mixing function G.
-
-#define B2S_G(a, b, c, d, x, y) {   \
-    v[a] = v[a] + v[b] + x;         \
-    v[d] = ROTR32(v[d] ^ v[a], 16); \
-    v[c] = v[c] + v[d];             \
-    v[b] = ROTR32(v[b] ^ v[c], 12); \
-    v[a] = v[a] + v[b] + y;         \
-    v[d] = ROTR32(v[d] ^ v[a], 8);  \
-    v[c] = v[c] + v[d];             \
-    v[b] = ROTR32(v[b] ^ v[c], 7); }
 
 // Initialization Vector.
-
 static const uint32_t blake2s_iv[8] =
 {
     0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
     0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
 };
 
-// Compression function. "last" flag indicates last block.
 
+
+//------------------------------------------------------------------
+// B2S_G macro redefined as a function.
+// Allows us to output intermediate values for debugging.
+//------------------------------------------------------------------
+void B2S_G(uint32_t *v, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t y) {
+    v[a] = v[a] + v[b] + x;
+    v[d] = ROTR32(v[d] ^ v[a], 16);
+    v[c] = v[c] + v[d];
+    v[b] = ROTR32(v[b] ^ v[c], 12);
+    v[a] = v[a] + v[b] + y;
+    v[d] = ROTR32(v[d] ^ v[a], 8);
+    v[c] = v[c] + v[d];
+    v[b] = ROTR32(v[b] ^ v[c], 7);
+}
+
+
+//------------------------------------------------------------------
+// Compression function. "last" flag indicates last block.
+//------------------------------------------------------------------
 static void blake2s_compress(blake2s_ctx *ctx, int last)
 {
     const uint8_t sigma[10][16] = {
-        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
-        { 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
-        { 11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4 },
-        { 7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8 },
-        { 9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13 },
-        { 2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9 },
-        { 12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11 },
-        { 13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10 },
-        { 6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5 },
-        { 10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0 }
+        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+        {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
+        {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
+        {7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
+        {9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
+        {2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
+        {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
+        {13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
+        {6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
+        {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0}
     };
+
     int i;
     uint32_t v[16], m[16];
 
-    for (i = 0; i < 8; i++) {           // init work variables
+    // init work variables
+    for (i = 0; i < 8; i++) {
         v[i] = ctx->h[i];
         v[i + 8] = blake2s_iv[i];
     }
 
-    v[12] ^= ctx->t[0];                 // low 32 bits of offset
-    v[13] ^= ctx->t[1];                 // high 32 bits
-    if (last)                           // last block flag set ?
+    // low 32 bits of offset
+    // high 32 bits
+    v[12] ^= ctx->t[0];
+    v[13] ^= ctx->t[1];
+
+    // last block flag set ?
+    if (last) {
         v[14] = ~v[14];
-
-    for (i = 0; i < 16; i++)            // get little-endian words
-        m[i] = B2S_GET32(&ctx->b[4 * i]);
-
-    for (i = 0; i < 10; i++) {          // ten rounds
-        B2S_G( 0, 4,  8, 12, m[sigma[i][ 0]], m[sigma[i][ 1]]);
-        B2S_G( 1, 5,  9, 13, m[sigma[i][ 2]], m[sigma[i][ 3]]);
-        B2S_G( 2, 6, 10, 14, m[sigma[i][ 4]], m[sigma[i][ 5]]);
-        B2S_G( 3, 7, 11, 15, m[sigma[i][ 6]], m[sigma[i][ 7]]);
-        B2S_G( 0, 5, 10, 15, m[sigma[i][ 8]], m[sigma[i][ 9]]);
-        B2S_G( 1, 6, 11, 12, m[sigma[i][10]], m[sigma[i][11]]);
-        B2S_G( 2, 7,  8, 13, m[sigma[i][12]], m[sigma[i][13]]);
-        B2S_G( 3, 4,  9, 14, m[sigma[i][14]], m[sigma[i][15]]);
     }
 
-    for( i = 0; i < 8; ++i )
+    // get little-endian words
+    for (i = 0; i < 16; i++) {
+        m[i] = B2S_GET32(&ctx->b[4 * i]);
+    }
+
+
+    // ten rounds
+    for (i = 0; i < 10; i++) {
+        B2S_G(&v[0], 0, 4,  8, 12, m[sigma[i][ 0]], m[sigma[i][ 1]]);
+        B2S_G(&v[0], 1, 5,  9, 13, m[sigma[i][ 2]], m[sigma[i][ 3]]);
+        B2S_G(&v[0], 2, 6, 10, 14, m[sigma[i][ 4]], m[sigma[i][ 5]]);
+        B2S_G(&v[0], 3, 7, 11, 15, m[sigma[i][ 6]], m[sigma[i][ 7]]);
+        B2S_G(&v[0], 0, 5, 10, 15, m[sigma[i][ 8]], m[sigma[i][ 9]]);
+        B2S_G(&v[0], 1, 6, 11, 12, m[sigma[i][10]], m[sigma[i][11]]);
+        B2S_G(&v[0], 2, 7,  8, 13, m[sigma[i][12]], m[sigma[i][13]]);
+        B2S_G(&v[0], 3, 4,  9, 14, m[sigma[i][14]], m[sigma[i][15]]);
+    }
+
+    for (i = 0; i < 8; ++i) {
         ctx->h[i] ^= v[i] ^ v[i + 8];
+    }
 }
 
+
+//------------------------------------------------------------------
 // Initialize the hashing context "ctx" with optional key "key".
 //      1 <= outlen <= 32 gives the digest size in bytes.
 //      Secret key (also <= 32 bytes) is optional (keylen = 0).
-
+//------------------------------------------------------------------
 int blake2s_init(blake2s_ctx *ctx, size_t outlen,
     const void *key, size_t keylen)     // (keylen=0: no key)
 {
@@ -115,8 +141,10 @@ int blake2s_init(blake2s_ctx *ctx, size_t outlen,
     return 0;
 }
 
-// Add "inlen" bytes from "in" into the hash.
 
+//------------------------------------------------------------------
+// Add "inlen" bytes from "in" into the hash.
+//------------------------------------------------------------------
 void blake2s_update(blake2s_ctx *ctx,
     const void *in, size_t inlen)       // data bytes
 {
@@ -134,20 +162,29 @@ void blake2s_update(blake2s_ctx *ctx,
     }
 }
 
+
+//------------------------------------------------------------------
 // Generate the message digest (size given in init).
 //      Result placed in "out".
-
+//------------------------------------------------------------------
 void blake2s_final(blake2s_ctx *ctx, void *out)
 {
     size_t i;
 
     ctx->t[0] += ctx->c;                // mark last block offset
-    if (ctx->t[0] < ctx->c)             // carry overflow
-        ctx->t[1]++;                    // high word
 
-    while (ctx->c < 64)                 // fill up with zeros
+    // carry overflow
+    // high word
+    if (ctx->t[0] < ctx->c) {
+        ctx->t[1]++;
+    }
+
+    // fill up with zeros
+    // final block flag = 1
+    while (ctx->c < 64) {
         ctx->b[ctx->c++] = 0;
-    blake2s_compress(ctx, 1);           // final block flag = 1
+    }
+    blake2s_compress(ctx, 1);
 
     // little endian convert and store
     for (i = 0; i < ctx->outlen; i++) {
@@ -156,8 +193,10 @@ void blake2s_final(blake2s_ctx *ctx, void *out)
     }
 }
 
-// Convenience function for all-in-one computation.
 
+//------------------------------------------------------------------
+// Convenience function for all-in-one computation.
+//------------------------------------------------------------------
 int blake2s(void *out, size_t outlen,
     const void *key, size_t keylen,
     const void *in, size_t inlen)
@@ -166,9 +205,13 @@ int blake2s(void *out, size_t outlen,
 
     if (blake2s_init(&ctx, outlen, key, keylen))
         return -1;
+
     blake2s_update(&ctx, in, inlen);
+
     blake2s_final(&ctx, out);
 
     return 0;
 }
 
+//======================================================================
+//======================================================================
