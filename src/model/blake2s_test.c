bf8812fe89b11711723730e742eaa04d889bd9d8
Joachim Strömbergson <joachim@assured.se>
2022-02-10 09:47:12 +0100

    Add test cases and functions to display msg, digest   (1) Functions for displaying digest and message.   (2) Test case for zero byte message matching Wikipedia result.   (3) Test case for 'abc' maessage matching RFC test vector.

--- src/model/blake2s_test.c
+++ src/model/blake2s_test.c
@@ -1,11 +1,17 @@
-// test_main.c
-// Self test Modules for BLAKE2b and BLAKE2s -- and a stub main().
+//======================================================================
+//
+// blake2s_test.c
+// --------------
+//
+//======================================================================
 
 #include <stdio.h>
 #include "blake2s.h"
 
+//------------------------------------------------------------------
+// selftest_seq
 // Deterministic sequences (Fibonacci generator).
-
+//------------------------------------------------------------------
 static void selftest_seq(uint8_t *out, size_t len, uint32_t seed)
 {
     size_t i;
@@ -22,60 +28,144 @@ static void selftest_seq(uint8_t *out, size_t len, uint32_t seed)
     }
 }
 
+
+//------------------------------------------------------------------
+// blake2s_selftest
 // BLAKE2s self-test validation. Return 0 when OK.
+//------------------------------------------------------------------
+int blake2s_selftest() {
+  // Grand hash of hash results.
+  const uint8_t blake2s_res[32] = {
+    0x6a, 0x41, 0x1f, 0x08, 0xce, 0x25, 0xad, 0xcd,
+    0xfb, 0x02, 0xab, 0xa6, 0x41, 0x45, 0x1c, 0xec,
+    0x53, 0xc5, 0x98, 0xb2, 0x4f, 0x4f, 0xc7, 0x87,
+    0xfb, 0xdc, 0x88, 0x79, 0x7f, 0x4c, 0x1d, 0xfe
+  };
 
-int blake2s_selftest()
-{
-    // Grand hash of hash results.
-    const uint8_t blake2s_res[32] = {
-        0x6A, 0x41, 0x1F, 0x08, 0xCE, 0x25, 0xAD, 0xCD,
-        0xFB, 0x02, 0xAB, 0xA6, 0x41, 0x45, 0x1C, 0xEC,
-        0x53, 0xC5, 0x98, 0xB2, 0x4F, 0x4F, 0xC7, 0x87,
-        0xFB, 0xDC, 0x88, 0x79, 0x7F, 0x4C, 0x1D, 0xFE
-    };
-    // Parameter sets.
-    const size_t b2s_md_len[4] = { 16, 20, 28, 32 };
-    const size_t b2s_in_len[6] = { 0,  3,  64, 65, 255, 1024 };
+  // Parameter sets.
+  const size_t b2s_md_len[4] = {16, 20, 28, 32};
+  const size_t b2s_in_len[6] = {0,  3,  64, 65, 255, 1024};
 
-    size_t i, j, outlen, inlen;
-    uint8_t in[1024], md[32], key[32];
-    blake2s_ctx ctx;
+  size_t i, j, outlen, inlen;
+  uint8_t in[1024], md[32], key[32];
+  blake2s_ctx ctx;
 
-    // 256-bit hash for testing.
-    if (blake2s_init(&ctx, 32, NULL, 0))
-        return -1;
+  // 256-bit hash for testing.
+  if (blake2s_init(&ctx, 32, NULL, 0))
+    return -1;
 
-    for (i = 0; i < 4; i++) {
-        outlen = b2s_md_len[i];
-        for (j = 0; j < 6; j++) {
-            inlen = b2s_in_len[j];
+  for (i = 0; i < 4; i++) {
+    outlen = b2s_md_len[i];
+    for (j = 0; j < 6; j++) {
+      inlen = b2s_in_len[j];
 
-            selftest_seq(in, inlen, inlen);     // unkeyed hash
-            blake2s(md, outlen, NULL, 0, in, inlen);
-            blake2s_update(&ctx, md, outlen);   // hash the hash
+      selftest_seq(in, inlen, inlen);     // unkeyed hash
+      blake2s(md, outlen, NULL, 0, in, inlen);
+      blake2s_update(&ctx, md, outlen);   // hash the hash
 
-            selftest_seq(key, outlen, outlen);  // keyed hash
-            blake2s(md, outlen, key, outlen, in, inlen);
-            blake2s_update(&ctx, md, outlen);   // hash the hash
-        }
+      selftest_seq(key, outlen, outlen);  // keyed hash
+      blake2s(md, outlen, key, outlen, in, inlen);
+      blake2s_update(&ctx, md, outlen);   // hash the hash
     }
+  }
 
-    // Compute and compare the hash of hashes.
-    blake2s_final(&ctx, md);
-    for (i = 0; i < 32; i++) {
-        if (md[i] != blake2s_res[i])
-            return -1;
-    }
+  // Compute and compare the hash of hashes.
+  blake2s_final(&ctx, md);
+  for (i = 0; i < 32; i++) {
+    if (md[i] != blake2s_res[i])
+      return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
-// Test driver.
 
-int main(int argc, char **argv)
-{
-    printf("blake2s_selftest() = %s\n",
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+void print_message(uint8_t *m, int mlen) {
+  printf("The message:\n");
+  for (int i = 0 ; i < mlen ; i++) {
+      printf("0x%02x ", m[i]);
+      if ((i > 0) && (!i % 8)) {
+        printf("\n");
+      }
+  }
+        printf("\n");
+
+}
+
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+void print_digest(uint8_t *md) {
+  printf("The digest:\n");
+  for (int j = 0 ; j < 4 ; j++) {
+    for (int i = 0 ; i < 8 ; i++) {
+      printf("0x%02x ", md[i + 8 * j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+
+//------------------------------------------------------------------
+// test_zero_length()
+// Test with a zero length mwssage.
+//------------------------------------------------------------------
+void test_zero_length() {
+
+  uint8_t md[32];
+
+  printf("Testing zero byte message.\n");
+  blake2s(md, 32, NULL, 0, NULL, 0);
+  print_digest(md);
+  printf("\n");
+}
+
+
+//------------------------------------------------------------------
+// test_abc_message()
+// Test with a zero length mwssage.
+//------------------------------------------------------------------
+void test_abc_message() {
+
+  uint8_t md[32];
+  uint8_t msg[64] = {'a', 'b', 'c'};
+
+  printf("Testing with three byte 'abc' message.\n");
+  print_message(msg, 3);
+
+  blake2s(md, 32, NULL, 0, msg, 3);
+  print_digest(md);
+  printf("\n");
+}
+
+
+//------------------------------------------------------------------
+// self_test
+// The original self test.
+//------------------------------------------------------------------
+void self_test() {
+  printf("blake2s_selftest() = %s\n",
          blake2s_selftest() ? "FAIL" : "OK");
-
-    return 0;
+  printf("\n");
 }
+
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+int main(void) {
+  printf("Blake2s reference model. Performing a set of tests..\n");
+
+  self_test();
+
+  test_zero_length();
+  test_abc_message();
+
+  return 0;
+}
+
+//======================================================================
+/// EOF blake2s_test.c
+//======================================================================
